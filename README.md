c901321cc1514ce7b7d9cca41d1a39fb5a494e11
Jander <jandertheodoro@gmail.com>
2023-11-12 14:05:38 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# JanGloom Theme
+![](./janGloom.png)
+
+## Description
+This theme was inspired by Synthwave and Sweet
+
+## contribution
+Contribua acessando o [github](https://www.openai.com/) do projeto.
---
+++ janGloom.js
@@ -0,0 +1,11 @@
+const JanGloom = {
+  name: 'JanGloom',
+  creator: 'Jander Teodoro',
+  age: 'Tem nem seis mêses' 
+}
+
+function tellTheTruth() {
+  return 'JanGloom is the best dark theme on the vscode 😎'
+}
+
+console.log(tellTheTruth())
